32cddef4429f6587c8ea1e0447710e32e8917c3a
Basavraj <111918104+basavraj19@users.noreply.github.com>
2023-10-26 14:01:30 +0530

    Time: 1 ms (95.18%), Space: 44 MB (21.81%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> ans =new ArrayList<List<Integer>>();
+    public List<List<Integer>> permute(int[] nums) {
+        ArrayList<Integer> arr=new ArrayList<>();
+        int b[]=new int[nums.length];
+        getPermutation(arr,nums,b);
+        return ans;
+    }
+    public void getPermutation(List<Integer> arr,int a[],int b[]){
+        if(arr.size()==a.length){
+            ans.add(new ArrayList<>(arr));
+            return;
+        }
+        for(int i=0;i<a.length;i++){
+            if(b[i]==1){
+                continue;
+            }
+            arr.add(a[i]);
+            b[i]=1;
+            getPermutation(arr,a,b);
+            arr.remove(arr.size()-1);
+            b[i]=0;
+        }
+    }
+}
