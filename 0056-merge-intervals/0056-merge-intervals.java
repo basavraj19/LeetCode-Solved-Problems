eaf29259b76d31dd1d1b5c1561d82e05550e0e10
Basavraj <111918104+basavraj19@users.noreply.github.com>
2023-10-28 13:56:54 +0530

    Time: 8 ms (91.74%), Space: 45.9 MB (15.53%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,40 +1,25 @@
 class Solution {
-    List<Integer> ans=new ArrayList<>();
     public int[][] merge(int[][] intervals) {
         if(intervals.length==1){
             return intervals;
         }
+        List<int[]> ans=new ArrayList<>();
         Arrays.sort(intervals,(a,b)->a[0]-b[0]);
-        solve(intervals);
-        int res[][]=new int[ans.size()/2][2];
-        int p=0;
-        for(int i=0;i<ans.size();i++){
-            res[p][0]=ans.get(i);
-            i++;
-            res[p][1]=ans.get(i);
-            p++;
-        }
-        return res;
+        int s=intervals[0][0];
+        int e=intervals[0][1];
+    
+         for(int a[]:intervals){
+             if(a[0]<=e){
+                 e=Math.max(e,a[1]);
+             }else
+             {
+                 ans.add(new int[]{s,e});
+                 s=a[0];
+                 e=a[1];
+             }
+         }
+        ans.add(new int[]{s,e});
+        return ans.toArray(new int[0][]);
     }
-    public void solve(int a[][]){
-        for(int i=0;i<a.length-1;i++){    
-            if(a[i][1]>=a[i+1][0]){
-                a[i+1][0]=Math.min(a[i+1][0],a[i][0]);
-                a[i+1][1]=Math.max(a[i][1],a[i+1][1]);
-            }else
-            {
-                ans.add(a[i][0]);
-                ans.add(a[i][1]);
-            }
-        }
-        int i=a.length-1;
-        if(a[i-1][1]>=a[i][0]){
-                ans.add(Math.min(a[i-1][0],a[i][0]));
-                ans.add(Math.max(a[i][1],a[i-1][1]));
-            }else
-            {
-                ans.add(a[i][0]);
-                ans.add(a[i][1]);
-            }
-    }
+
 }
