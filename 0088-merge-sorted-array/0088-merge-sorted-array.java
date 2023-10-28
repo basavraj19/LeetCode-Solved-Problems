0e51aa86b759a7002abbe46101dd653a0b28b7a3
Basavraj <111918104+basavraj19@users.noreply.github.com>
2023-10-28 21:00:13 +0530

    Time: 1 ms (36.77%), Space: 41.7 MB (8.77%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,23 +1,34 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-    int r=m+n-1;
-    m=m-1;
-    n=n-1;
-       while(m>=0&&n>=0){
-          if(nums1[m]>nums2[n]){
-              nums1[r]=nums1[m];
-              m--;
-          }else
-          {
-              nums1[r]=nums2[n];
-              n--;
-          }
-           r--;
-       }
-       while(n>=0){
-           nums1[r]=nums2[n];
-           r--;
-           n--;
-       }
+        int j=0;
+        for(int i=m;i<m+n;i++){
+            nums1[i]=nums2[j];
+            j++;
+        }
+        int gap=(int)Math.ceil((m+n)/2);
+        int l=0,r=gap;
+        while(gap>0){
+            while(r<m+n){
+                if(nums1[l]>nums1[r]){
+                    int t=nums1[l];
+                    nums1[l]=nums1[r];
+                    nums1[r]=t;
+                }
+                l++;r++;
+            }
+            gap=(int)Math.ceil(gap/2);
+            l=0;
+            r=gap;
+        }
+        l=0;r=1;
+        while(r<m+n){
+                if(nums1[l]>nums1[r]){
+                    int t=nums1[l];
+                    nums1[l]=nums1[r];
+                    nums1[r]=t;
+                }
+                l++;r++;
+            }
+        
     }
 }
