eb45358d01d394a46c0e57f09d488172514f750f
Basavraj <111918104+basavraj19@users.noreply.github.com>
2023-11-01 20:29:40 +0530

    Time: 2 ms (97.37%), Space: 47.2 MB (8.12%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,32 +1,33 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> ans=new ArrayList<>();
-        int c1=0,c2=0,x=0,y=0;
+        int c1=0,c2=0,e1=0,e2=0;
         for(int i=0;i<nums.length;i++){
-            if(x==0&&nums[i]!=c2){
-                x++;
-                c1=nums[i];
-            }else if(y==0&&nums[i]!=c1){
-                y++;
-                c2=nums[i];
-            }else if(nums[i]==c1){
-                x++;
-            }else if(nums[i]==c2){
-                y++;
-            }else
-            {
-                x--;
-                y--;
+            if(c1==0&&nums[i]!=e2){
+                c1++;
+                e1=nums[i];
+            }else if(c2==0&&nums[i]!=e1){
+                c2++;
+                e2=nums[i];
+            }else if(nums[i]==e1){
+                c1++;
+            }else if(nums[i]==e2){
+                c2++;
+            }else{
+                c1--;
+                c2--;
             }
         }
-        x=0;y=0;
+        c1=0;c2=0;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==c1) x++;
-            else if(nums[i]==c2) y++;
+            if(nums[i]==e1){
+                c1++;
+            }else if(nums[i]==e2){
+                c2++;
+            }
         }
-        if(x>nums.length/3) ans.add(c1);
-        if(y>nums.length/3) ans.add(c2);
-        
+        if(c1>nums.length/3) ans.add(e1);
+        if(c2>nums.length/3) ans.add(e2);
         return ans;
     }
 }
