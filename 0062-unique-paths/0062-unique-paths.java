73014972cfeb157ef48c162249d5375a47630156
Basavraj <111918104+basavraj19@users.noreply.github.com>
2023-11-02 13:15:48 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (28.29%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,22 +1,11 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int arr [][] =new int[m][n];
-        int r= solve(m,n,0,0,arr);
-        return r;
+     int N=m+n-2;
+     int r=m-1;
+    double q=1;
+    for(int i=1;i<=r;i++){
+        q=q*(N-r+i)/i;
     }
-    public int solve(int m,int n,int r,int d,int arr[][]){
-        if(r==n-1&&d==m-1){
-            return 1;
-        }
-        if(r>=n||d>=m){
-            return 0;
-        }
-        if(arr[d][r]!=0){
-            return arr[d][r];
-        }
-        int p=solve(m,n,r+1,d,arr);
-        int q=solve(m,n,r,d+1,arr);
-        arr[d][r]=p+q;
-        return p+q;
+        return (int)q;
     }
 }
