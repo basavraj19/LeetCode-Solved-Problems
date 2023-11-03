0e732d42bfa69fddb7dc50208cd71989bacb6da8
Basavraj <111918104+basavraj19@users.noreply.github.com>
2023-11-03 23:42:08 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (90.24%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -1,17 +1,16 @@
 class Solution {
     public List<String> buildArray(int[] target, int n) {
-        HashMap<Integer,Integer> map=new HashMap<>(); 
         List<String> ans=new ArrayList<>();
-      for(int i=0;i<target.length;i++){
-          map.put(target[i],1);
-      }  
-    for(int i=1;i<=n;i++){
-      ans.add("Push");
-      if(!map.containsKey(i)){
-          ans.add("Pop");
-      }
-        if(target[target.length-1]==i) return ans;
-    }
-        return ans;
+        int j=0;
+        for(int i=1;i<=n;i++){
+            ans.add("Push");
+            if(!(target[j]==i)){
+                ans.add("Pop");
+             }else{
+                  j++;   
+             }
+             if(target[target.length-1]==i) return ans;
+        }
+            return ans;
     }
 }
