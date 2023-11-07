2263054cab6e5db1553e4bf64483ab7606496bc8
Basavraj <111918104+basavraj19@users.noreply.github.com>
2023-11-07 23:29:51 +0530

    Time: 19 ms (43.17%), Space: 43.6 MB (86.88%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,29 +1,31 @@
 class Solution {
-    public List<List<Integer>> fourSum(int[] a, int target) {
-        List<List<Integer>> ans=new ArrayList<List<Integer>>();
-        Arrays.sort(a);
-        for(int i=0;i<a.length;i++){
-            if(i>0&&a[i]==a[i-1]){
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> ans=new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(i>0&&nums[i]==nums[i-1]){
                 continue;
             }
-            for(int j=i+1;j<a.length;j++){
-                if(j>i+1&&a[j]==a[j-1]) continue;
-                int k=j+1,l=a.length-1;
+            for(int j=i+1;j<nums.length;j++){
+                 if(j>i+1&&nums[j]==nums[j-1]){
+                continue;
+            }
+                int k=j+1,l=nums.length-1;
                 while(k<l){
-                    long s=(long)a[i]+a[j]+a[k]+a[l];
-                    if(s==(long)target){
-                        ans.add(Arrays.asList(a[i],a[j],a[k],a[l]));  
-                        while(k<l&&a[k]==a[k+1]){
-                            k++;
-                        }
-                        while(l>k&&a[l]==a[l-1]){
-                            l--;
-                        }
+                    long sum=(long)nums[i]+nums[j]+nums[k]+nums[l];
+                    if(sum==target){
+                       ans.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
+                    while(k<l&&nums[k]==nums[k+1]){
                         k++;
+                    }
+                    while(k<l&&nums[l]==nums[l-1]){
                         l--;
-                    }else if(s>target){
+                    }
+                       k++;
+                       l--;
+                    }else if(sum>target){
                         l--;
-                    }else {
+                    }else{
                         k++;
                     }
                 }
