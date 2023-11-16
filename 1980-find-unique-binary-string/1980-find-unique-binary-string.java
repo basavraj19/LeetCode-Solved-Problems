95bf3283d94af02f9d85eb2f0147a729ddd5db40
Basavraj <111918104+basavraj19@users.noreply.github.com>
2023-11-17 00:21:02 +0530

    Time: 0 ms (100.00%), Space: 40 MB (83.82%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.java
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -1,21 +1,15 @@
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
-        HashMap<String,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],i);
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < nums.length; ++i) {
+            char currentChar = nums[i].charAt(i);
+            char oppositeChar = (currentChar == '0') ? '1' : '0';
+
+            // Append the opposite character to the result string
+            ans.append(oppositeChar);
         }
-        return solve(nums.length,map,"");
-    }
-    public String solve(int n,HashMap<String,Integer>map,String s){
-        if(s.length()==n){
-            if(!map.containsKey(s)){
-                return s;
-            }
-            return "";
-        }
-        String r= solve(n,map,s+'0');
-        if (!r.isEmpty()) return r;
-        return solve(n,map,s+'1');
-        
+
+        return ans.toString();
     }
 }
