c936393e0255cc59579c77a2327ce9fe046365fd
Basavraj <111918104+basavraj19@users.noreply.github.com>
2023-11-20 01:03:02 +0530

    Time: 42 ms (70.12%), Space: 46 MB (50.61%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.java
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.java
@@ -1,33 +1,21 @@
 class Solution {
     public String sortVowels(String s) {
-        int a[]=new int[52];
+        ArrayList<Character> a=new ArrayList<>();
+        int p=0;
         char ch[]=s.toCharArray();
         for(int i=0;i<s.length();i++){
            if(isVowel(ch[i])){
-              if(ch[i]>=65&&ch[i]<=92){
-                  a[ch[i]-'A']+=1;
-              }else{
-                  a[ch[i]-'a'+26]+=1;
-              }
+             a.add(ch[i]);
            }
         }
-        for(int i=0;i<s.length();i++){
+        Collections.sort(a);
+        for(int i=0;i<ch.length;i++){
             if(isVowel(ch[i])){
-                int k=0;
-                while(k<a.length){
-                    if(a[k]!=0){
-                        a[k]-=1;
-                        break;
-                    }
-                    k++;
-                }
-                 if(k<=25){
-                 ch[i]=(char)(k+65);
-                 }else {
-                 ch[i]=(char)(k+97-26);
-                 }
+                ch[i]=a.get(p);
+                p++;
             }
         }
+        
          return new String(ch);  
     }
     public boolean isVowel(char c){
