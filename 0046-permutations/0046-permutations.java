d63a4d9c626591e613b346cad11641dc7bf67bad
Basavraj <111918104+basavraj19@users.noreply.github.com>
2023-11-24 16:44:08 +0530

    Time: 0 ms (100.00%), Space: 43.9 MB (44.36%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,23 +1,25 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans=new ArrayList<List<Integer>>();
-        List<Integer> arr=new ArrayList<>();
-        int b[]=new int[nums.length];
-        return solve(nums,b,ans,arr);
+        return solve(nums,0,ans);
     }
-    public List<List<Integer>> solve(int a[],int b[],List<List<Integer>> ans,List<Integer> arr){
-        if(arr.size()==a.length){
-            ans.add(new ArrayList<Integer>(arr));
+    public List<List<Integer>> solve(int a[],int j,List<List<Integer>> ans){
+        if(j==a.length){
+            List<Integer> arr =new ArrayList<>();
+            for(int i=0;i<a.length;i++){
+                arr.add(a[i]);
+            }
+            ans.add(arr);
             return ans;
         }
-        for(int i=0;i<a.length;i++){
-            if(b[i]!=1){
-                b[i]=1;
-                arr.add(a[i]);
-                ans=solve(a,b,ans,arr);
-                b[i]=0;
-                arr.remove(arr.size()-1);
-            }
+        for(int i=j;i<a.length;i++){
+            int t=a[i];
+            a[i]=a[j];
+            a[j]=t;
+            solve(a,j+1,ans);
+            t=a[i];
+            a[i]=a[j];
+            a[j]=t;
         }
         return ans;
     }
