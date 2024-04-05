8fa8e6a917daa225e18a3dee9d78d1a343cefa43
Basavraj <111918104+basavraj19@users.noreply.github.com>
2024-04-06 00:40:59 +0530

    Time: 5 ms (14.52%), Space: 42.7 MB (18.77%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,19 +1,17 @@
 class Solution {
     public String makeGood(String s) {
-        int i=0;
-        while(i<s.length()-1){
-            int val1=(int)s.charAt(i);
-            int val2=(int)s.charAt(i+1);
-            if(Math.abs(val1-val2)==32){
-              s=s.substring(0,i)+s.substring(i+2,s.length());
-              if(i>0){
-                  i--;
-              }else{
-                  i=0;
-              }
-              continue;
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(!stack.isEmpty()&&Math.abs(s.charAt(i)-stack.peek())==32){
+                stack.pop();
+            }else{
+                stack.push(s.charAt(i));
             }
-            i++;
+        }
+        s="";
+        while(stack.size()>0){
+            s=stack.peek()+s;
+            stack.pop();
         }
         return s;
     }
