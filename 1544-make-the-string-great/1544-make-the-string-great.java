110d66982784920eb5949527e2c213368e27a395
Basavraj <111918104+basavraj19@users.noreply.github.com>
2024-04-06 00:18:20 +0530

    Time: 3 ms (61.07%), Space: 42.5 MB (25.66%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String makeGood(String s) {
+        while(true){
+            int res=isGoodString(s);
+            if(res==-1)
+            {
+                return s;
+            }else{
+                s=s.substring(0,res)+s.substring(res+2,s.length());
+            }
+        }
+    }
+    public int isGoodString(String s){
+        for(int i=0;i<s.length()-1;i++){
+            int val1=(int)s.charAt(i);
+            int val2=(int)s.charAt(i+1);
+            val1=val1-val2;
+            if(Math.abs(val1)==32){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
