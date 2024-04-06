b6993ebacb66c2c44a737efba305927294a423c0
Basavraj <111918104+basavraj19@users.noreply.github.com>
2024-04-07 00:22:33 +0530

    Time: 607 ms (5.07%), Space: 45.9 MB (9.53%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,26 +1,35 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        boolean remove[]=new boolean[s.length()];
-        Stack<Integer> stack=new Stack<>();
+        int open=0,close=0,flag=0;
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='('){
-                stack.push(i);
-            }else if(s.charAt(i)==')'){
-                if(stack.size()>0){
-                    stack.pop();
-                }else{
-                    remove[i]=true;
-                }
+                open++;
+                flag++;
+            }else if(s.charAt(i)==')'&&flag>0){
+                close++;
+                flag--;
             }
         }
-        while(stack.size()>0){
-           remove[stack.pop()]=true; 
-        }
+        int min=Math.min(open,close);
         String ans="";
+        open=min;
+        close=min;
         for(int i=0;i<s.length();i++){
-            if(!remove[i]){
-                ans+=s.charAt(i);
+            if(s.charAt(i)=='('){
+                if(open>0){
+                    ans+='(';
+                    open--;
+                }
+                continue;
             }
+            else if(s.charAt(i)==')'){
+                if(close>0&&close>open){
+                    ans+=')';
+                    close--;
+                }
+                continue;
+            }
+            ans+=s.charAt(i);
         }
         return ans;
     }
