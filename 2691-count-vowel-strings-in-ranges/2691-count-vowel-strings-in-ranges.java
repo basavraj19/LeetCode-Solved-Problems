64de099e2cb0a71d7699a0d6e7e421a8d7c9f138
Basavraj <111918104+basavraj19@users.noreply.github.com>
2025-01-03 00:21:46 +0530

    Time: 15 ms (11.16%), Space: 85.7 MB (63.33%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
@@ -4,17 +4,14 @@ class Solution {
         int a[]= check(words);
         
         for(int i=0;i<queries.length;i++){
-         ans[i]=count(a,queries[i][0],queries[i][1]);
+         int j=queries[i][0];
+         if(j==0){
+           ans[i]=a[queries[i][1]]; 
+         }else{
+           ans[i]=a[queries[i][1]]-a[j-1];
+         }  
         }
         return ans;
-    }
-
-    public int count(int a[],int start,int end){
-        int count=0;
-        for(int i=start;i<=end;i++){
-            count+=a[i];
-        }
-        return count;
     }
 
     public int[] check(String[] words){
@@ -22,9 +19,18 @@ class Solution {
         for(int i=0;i<words.length;i++){
             int s=words[i].length()-1;
             if((words[i].charAt(0)=='a'||words[i].charAt(0)=='e'||words[i].charAt(0)=='i'||words[i].charAt(0)=='o'||words[i].charAt(0)=='u')&&(words[i].charAt(s)=='a'||words[i].charAt(s)=='e'||words[i].charAt(s)=='i'||words[i].charAt(s)=='o'||words[i].charAt(s)=='u')){
-               a[i]=1;
+               if(i==0){
+                 a[i]=1;
+               }else{
+                 a[i]=a[i-1]+1;
+               }
+            }else{
+                if(i==0){
+                 a[i]=0;
+               }else{
+                 a[i]=a[i-1];
+               }
             }
-            
         }
         return a;
     }
