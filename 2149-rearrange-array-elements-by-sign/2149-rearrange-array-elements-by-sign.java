1ba02825cb2609968a5c0f8ab259e9c9df5b1a26
Basavraj <111918104+basavraj19@users.noreply.github.com>
2026-03-15 11:55:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,25 +1,22 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        List<Integer> posList = new ArrayList<>();
-        List<Integer> negList = new ArrayList<>();
+       List<Integer> posList = new ArrayList<>();
+       List<Integer> negList = new ArrayList<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                posList.add(nums[i]);
-            } else {
-                negList.add(nums[i]);
-            }
+       for(int i=0;i<nums.length;i++){
+        if(nums[i] >0) {
+            posList.add(nums[i]);
+        }else{
+            negList.add(nums[i]);
         }
+       }
 
-        int i = 0, j = 0;
-        for (int k = 0; k < nums.length; k++) {
-            if (k % 2 == 0) {
-                nums[k] = posList.get(i++);
-            } else {
-                nums[k] = negList.get(j++);
-            }
-        }
+       int i=0,j=0;
+       for(int k=0;k<nums.length/2;k++) {
+          nums[2*k] = posList.get(i++);
+          nums[2*k+1] = negList.get(j++);
+       }
 
-        return nums;
+       return nums;
     }
 }
