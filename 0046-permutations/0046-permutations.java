80f837b56c9ce4cf3a5159e07ea6f5147dff5816
Basavraj <111918104+basavraj19@users.noreply.github.com>
2026-03-16 00:26:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -3,24 +3,24 @@ class Solution {
         int vis[] = new int[nums.length];
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> arr = new ArrayList<>();
-        return getAllPermutations(nums, vis, arr, ans);
-     }
+        getAllPermutations(nums, vis, arr, ans);
+        return ans;
+    }
 
-    public List<List<Integer>> getAllPermutations(int nums[], int vis[], List<Integer> arr, List<List<Integer>> ans) {
-        if(arr.size() == nums.length) {
+    public void getAllPermutations(int nums[], int vis[], List<Integer> arr, List<List<Integer>> ans) {
+        if (arr.size() == nums.length) {
             ans.add(new ArrayList<>(arr));
-            return ans;
+            return;
         }
 
-        for(int i=0;i<nums.length;i++) {
-            if(vis[i] != 1) {
+        for (int i = 0; i < nums.length; i++) {
+            if (vis[i] != 1) {
                 arr.add(nums[i]);
                 vis[i] = 1;
                 ans = getAllPermutations(nums, vis, arr, ans);
                 vis[i] = 0;
-                arr.remove(arr.size()-1);
+                arr.remove(arr.size() - 1);
             }
         }
-           return ans;
-     }
+    }
 }
