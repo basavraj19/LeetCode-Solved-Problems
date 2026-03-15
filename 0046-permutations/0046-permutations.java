f99d74b64c38074a714d7e51298194e71aae46fd
Basavraj <111918104+basavraj19@users.noreply.github.com>
2026-03-16 00:25:12 +0530

    Time: 1 ms (85.21%), Space: 45.6 MB (60.56%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,26 +1,26 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<List<Integer>>();
-        return solve(nums,0,ans);
-    }
-    public List<List<Integer>> solve(int a[],int j,List<List<Integer>> ans){
-        if(j==a.length){
-            List<Integer> arr =new ArrayList<>();
-            for(int i=0;i<a.length;i++){
-                arr.add(a[i]);
-            }
-            ans.add(arr);
+        int vis[] = new int[nums.length];
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> arr = new ArrayList<>();
+        return getAllPermutations(nums, vis, arr, ans);
+     }
+
+    public List<List<Integer>> getAllPermutations(int nums[], int vis[], List<Integer> arr, List<List<Integer>> ans) {
+        if(arr.size() == nums.length) {
+            ans.add(new ArrayList<>(arr));
             return ans;
         }
-        for(int i=j;i<a.length;i++){
-            int t=a[i];
-            a[i]=a[j];
-            a[j]=t;
-            solve(a,j+1,ans);
-            t=a[i];
-            a[i]=a[j];
-            a[j]=t;
+
+        for(int i=0;i<nums.length;i++) {
+            if(vis[i] != 1) {
+                arr.add(nums[i]);
+                vis[i] = 1;
+                ans = getAllPermutations(nums, vis, arr, ans);
+                vis[i] = 0;
+                arr.remove(arr.size()-1);
+            }
         }
-        return ans;
-    }
+           return ans;
+     }
 }
