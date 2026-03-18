7ee32e7497f6abcc5c1e3cf298ae2f7df99a1e1d
Basavraj <111918104+basavraj19@users.noreply.github.com>
2026-03-19 02:01:24 +0530

    Time: 0 ms (100%), Space: 44.9 MB (11.88%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,37 +1,37 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-      int idx=-1;
-       for(int i=0;i<nums.length-1;i++){
-           if(nums[i]<nums[i+1]){
-               idx=i;
-           }
-       }
-        if(idx==-1){
-            int i=0,j=nums.length-1;
-        while(i<=j){
-            int t=nums[i];
-            nums[i]=nums[j];
-            nums[j]=t;
-            i++;
-            j--;
-        }
-            return;
-        }
-        for(int i=nums.length-1;i>idx;i--){
-            if(nums[idx]<nums[i]){
-                int t=nums[i];
-                nums[i]=nums[idx];
-                nums[idx]=t;
+        int ind = -1;
+
+        for (int i = nums.length - 1; i > 0; i--) {
+            if (nums[i - 1] < nums[i]) {
+                ind = i - 1;
                 break;
             }
         }
-        int i=idx+1,j=nums.length-1;
-        while(i<=j){
-            int t=nums[i];
-            nums[i]=nums[j];
-            nums[j]=t;
+
+        if (ind == -1) {
+            reverse(nums, 0, nums.length - 1);
+            return;
+        }
+
+        for (int i = nums.length - 1; i > ind; i--) {
+            if (nums[i] > nums[ind]) {
+                int t = nums[i];
+                nums[i] = nums[ind];
+                nums[ind] = t;
+                break;
+            }
+        }
+        reverse(nums, ind + 1, nums.length - 1);
+    }
+
+    public void reverse(int nums[], int i, int j) {
+        while (i <= j) {
+            int t = nums[i];
+            nums[i] = nums[j];
+            nums[j] = t;
             i++;
             j--;
         }
-    } 
+    }
 }
