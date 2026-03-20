255c74366957d490a733c2741e7a0157f5b05cfd
Basavraj <111918104+basavraj19@users.noreply.github.com>
2026-03-21 00:58:06 +0530

    Time: 81 ms (5.05%), Space: 91.2 MB (77.04%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -3,19 +3,29 @@ class Solution {
         if (nums.length == 0) {
             return 0;
         }
+       
+        TreeSet<Integer>  set = new TreeSet<>();
+        for (int i = 0; i < nums.length; i++) {
+           set.add(nums[i]);
+        }
+        
         int count = 1, max = 1;
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 1; i++) {
-            if(nums[i + 1] - nums[i] == 0) {
+        Integer cur = set.first();
+
+        for(Integer val : set) {
+            if(val-cur == 0) {
+                cur = val;
                 continue;
-            }else if (nums[i + 1] - nums[i] == 1) {
+            }
+            if(val-cur == 1) {
                 count++;
-                max = Math.max(max, count);
-            } else {
+                max  = Math.max(max, count);
+            }else{
                 count = 1;
             }
+            cur = val;
         }
-
+      
         return max;
     }
 }
