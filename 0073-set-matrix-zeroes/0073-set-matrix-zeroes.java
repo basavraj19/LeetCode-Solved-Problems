ba5c44998460fb90a1fc87b52cfae8494c262896
Basavraj <111918104+basavraj19@users.noreply.github.com>
2026-03-22 00:34:54 +0530

    Time: 1 ms (68.19%), Space: 47.3 MB (73.42%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,30 +1,21 @@
 class Solution {
     public void setZeroes(int[][] m) {
-        List<Integer> rowZero = new ArrayList<>();
-        List<Integer> colZero = new ArrayList<>();
-
-        for (int i = 0; i < m.length; i++) {
-            for (int j = 0; j < m[0].length; j++) {
-                if (m[i][j] == 0) {
-                    if (!rowZero.contains(i))
-                        rowZero.add(i);
-                    if (!colZero.contains(j))
-                        colZero.add(j);
+        int row[] = new int [m.length];
+        int col[] = new int [m[0].length];
+        for(int i=0;i<m.length;i++) {
+            for(int j=0;j<m[0].length;j++) {
+                if(m[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
 
-        for (int i = 0; i < rowZero.size(); i++) {
-            int ind = rowZero.get(i);
-            for (int j = 0; j < m[0].length; j++) {
-                m[ind][j] = 0;
-            }
-        }
-
-        for (int i = 0; i < colZero.size(); i++) {
-            int ind = colZero.get(i);
-            for (int j = 0; j < m.length; j++) {
-                m[j][ind] = 0;
+        for(int i=0;i<m.length;i++) {
+            for(int j=0;j<m[0].length;j++) {
+                if(row[i] == 1 || col[j] == 1) {
+                    m[i][j] = 0;
+                }
             }
         }
     }
