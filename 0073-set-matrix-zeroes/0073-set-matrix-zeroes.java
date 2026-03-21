1aed7ae45867ddc57b798e461c08007ab69c3684
Basavraj <111918104+basavraj19@users.noreply.github.com>
2026-03-21 20:51:34 +0530

    Time: 12 ms (5.04%), Space: 47.5 MB (55.35%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,27 +1,28 @@
 class Solution {
-    public void setZeroes(int[][] matrix) {
-       Set<Integer> rowSet = new HashSet<>();
-       Set<Integer> colSet = new HashSet<>();
-
-       for(int i=0;i<matrix.length;i++){
-        for(int j=0;j<matrix[0].length;j++){
-            if(matrix[i][j]==0){
-                rowSet.add(i);
-                colSet.add(j);
+    public void setZeroes(int[][] m) {
+        int a[][] = new int[m.length][m[0].length];
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                a[i][j] = m[i][j];
             }
         }
-       }
-       
-       for(int ele:rowSet){
-        for(int i=0;i<matrix[0].length;i++){
-            matrix[ele][i]=0;
-        }
-       }
 
-    for(int ele:colSet){
-        for(int i=0;i<matrix.length;i++){
-            matrix[i][ele]=0;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                if (a[i][j] == 0) {
+                    solve(m, i, j);
+                }
+            }
         }
-       }
+    }
+
+    public void solve(int m[][], int row, int col) {
+        for (int i = 0; i < m[0].length; i++) {
+            m[row][i] = 0;
+        }
+
+        for (int i = 0; i < m.length; i++) {
+            m[i][col] = 0;
+        }
     }
 }
