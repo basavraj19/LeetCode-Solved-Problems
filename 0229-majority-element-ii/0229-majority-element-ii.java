958b0a1382503f2ba40e5b40f0ace3915877c3dd
Basavraj <111918104+basavraj19@users.noreply.github.com>
2026-04-22 00:23:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,16 +1,50 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer, Integer>  map = new HashMap<>();
-        List<Integer>  ans = new ArrayList<>();
-        for(int i=0;i<nums.length;i++) {
-           map.put(nums[i], map.getOrDefault(nums[i],0) +1);
-        }
 
-        for(Map.Entry<Integer, Integer> m : map.entrySet()) {
-            if(m.getValue() > nums.length/3) {
-                ans.add(m.getKey());
+        if (nums.length == 1) {
+            return new ArrayList<>(Arrays.asList(nums[0]));
+        }
+        int e1 = nums[0], e2 = nums[0], ec1 = 0, ec2 = 0;
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (ec1 == 0 && nums[i] != e2) {
+                ec1 = 0;
+                e1 = nums[i];
+            }
+            if (ec2 == 0 && nums[i] != e1) {
+                ec2 = 0;
+                e2 = nums[i];
+            }
+
+            if (e1 == nums[i]) {
+                ec1++;
+            } else if (e2 == nums[i]) {
+                ec2++;
+            } else {
+                ec1--;
+                ec2--;
             }
         }
+
+        ec1 = 0;
+        ec2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == e1) {
+                ec1++;
+            }
+            if (nums[i] == e2) {
+                ec2++;
+            }
+        }
+
+        if (ec1 > nums.length / 3) {
+            ans.add(e1);
+        }
+
+        if (ec2 > nums.length / 3) {
+            ans.add(e2);
+        }
+
         return ans;
     }
 }
