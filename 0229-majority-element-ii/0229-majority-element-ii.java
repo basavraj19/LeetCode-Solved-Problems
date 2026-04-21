25a8ca19bf6b7e515577ced420ae4bfad9fd6534
Basavraj <111918104+basavraj19@users.noreply.github.com>
2026-04-22 01:33:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,43 +1,33 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int e1 = nums[0], e2 = nums[0], ec1 = 0, ec2 = 0;
-        List<Integer> ans = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (ec1 == 0 && nums[i] != e2) {
-                ec1 = 1;
-                e1 = nums[i];
-            } else if (ec2 == 0 && nums[i] != e1) {
-                ec2 = 1;
-                e2 = nums[i];
-            } else if (e1 == nums[i]) {
-                ec1++;
-            } else if (e2 == nums[i]) {
-                ec2++;
-            } else {
-                ec1--;
-                ec2--;
+        List<Integer> ans=new ArrayList<>();
+        int c1=0,c2=0,e1=0,e2=0;
+        for(int i=0;i<nums.length;i++){
+            if(c1==0&&nums[i]!=e2){
+                c1++;
+                e1=nums[i];
+            }else if(c2==0&&nums[i]!=e1){
+                c2++;
+                e2=nums[i];
+            }else if(nums[i]==e1){
+                c1++;
+            }else if(nums[i]==e2){
+                c2++;
+            }else{
+                c1--;
+                c2--;
             }
         }
-
-        ec1 = 0;
-        ec2 = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == e1) {
-                ec1++;
-            }
-            if (nums[i] == e2) {
-                ec2++;
+        c1=0;c2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==e1){
+                c1++;
+            }else if(nums[i]==e2){
+                c2++;
             }
         }
-
-        if (ec1 > nums.length / 3 && !ans.contains(e1)) {
-            ans.add(e1);
-        }
-
-        if (ec2 > nums.length / 3 && !ans.contains(e2)) {
-            ans.add(e2);
-        }
-
+        if(c1>nums.length/3) ans.add(e1);
+        if(c2>nums.length/3) ans.add(e2);
         return ans;
     }
 }
