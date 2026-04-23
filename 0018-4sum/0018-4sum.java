5cd1030753c35dc0af73fd382db1062ea2036999
Basavraj <111918104+basavraj19@users.noreply.github.com>
2026-04-23 23:29:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,33 +1,21 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> ans=new ArrayList<>();
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++){
-            if(i>0&&nums[i]==nums[i-1]){
-                continue;
-            }
-            for(int j=i+1;j<nums.length;j++){
-                 if(j>i+1&&nums[j]==nums[j-1]){
-                continue;
-            }
-                int k=j+1,l=nums.length-1;
-                while(k<l){
-                    long sum=(long)nums[i]+nums[j]+nums[k]+nums[l];
-                    if(sum==target){
-                       ans.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
-                    while(k<l&&nums[k]==nums[k+1]){
-                        k++;
+        int n = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                Map<Long, Integer> map = new HashMap<>();
+                for (int k = j + 1; k < n; k++) {
+                    Long s = (long) nums[i] + nums[j] + nums[k];
+                    long x = (long) target - s;
+                    if (map.containsKey(x)) {
+                        List<Integer> arr = new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[k], (int) x));
+                        Collections.sort(arr);
+                        if (!ans.contains(arr)) {
+                            ans.add(arr);
+                        }
                     }
-                    while(k<l&&nums[l]==nums[l-1]){
-                        l--;
-                    }
-                       k++;
-                       l--;
-                    }else if(sum>target){
-                        l--;
-                    }else{
-                        k++;
-                    }
+                    map.put((long) nums[k], 1);
                 }
             }
         }
