6422fa2d1fcd2028d754dd3752ba1836d6d59226
Basavraj <111918104+basavraj19@users.noreply.github.com>
2026-04-26 14:34:05 +0530

    Time: 8 ms (91.18%), Space: 49.1 MB (40.99%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -4,43 +4,34 @@ class Solution {
             return intervals;
         }
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-
-        List<List<Integer>> ans = new ArrayList<>();
-        int n=0;
+        int n = 0, p = 0;
         int start = intervals[0][0], end = intervals[0][1];
 
         for (int i = 1; i < intervals.length; i++) {
             if (intervals[i][0] <= end) {
                 end = Math.max(intervals[i][1], end);
             } else {
-               n++;
+                end = intervals[i][1];
+                n++;
             }
         }
-
+        int res[][] = new int[n + 1][2];
+        start = intervals[0][0];
+        end = intervals[0][1];
         for (int i = 1; i < intervals.length; i++) {
             if (intervals[i][0] <= end) {
                 end = Math.max(intervals[i][1], end);
                 start = Math.min(intervals[i][0], start);
             } else {
-                List<Integer> arr = new ArrayList<>();
-                arr.add(start);
-                arr.add(end);
-                ans.add(arr);
+                res[p][0] = start;
+                res[p][1] = end;
+                p++;
                 start = intervals[i][0];
                 end = intervals[i][1];
             }
         }
-
-        List<Integer> arr = new ArrayList<>();
-        arr.add(start);
-        arr.add(end);
-        ans.add(arr);
-
-        int res[][] = new int[n+1][2];
-        for (int i = 0; i < n+1; i++) {
-            res[i][0] = ans.get(i).get(0);
-            res[i][1] = ans.get(i).get(1);
-        }
+        res[p][0] = start;
+        res[p][1] = end;
 
         return res;
     }
