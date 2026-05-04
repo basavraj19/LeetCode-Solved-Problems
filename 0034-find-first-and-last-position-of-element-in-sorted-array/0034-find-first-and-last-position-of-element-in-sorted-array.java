37a393a68a8d488c45736ee5b23d2464697f8a1a
Basavraj <111918104+basavraj19@users.noreply.github.com>
2026-05-05 02:23:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -2,19 +2,28 @@ class Solution {
     public int[] searchRange(int[] nums, int target) {
         int[] ans = new int[] { -1, -1 };
         int i = 0, j = nums.length - 1;
-        while (i < nums.length && j >= 0) {
-            if (nums[i] == target && ans[0] == -1) {
-                ans[0] = i;
+        while (i <= j) {
+            int mid = (i + j) / 2;
+            if (nums[mid] == target) {
+                j = mid - 1;
+                ans[0] = mid;
+            } else if (nums[mid] > target) {
+                j = mid - 1;
+            } else {
+                i = mid + 1;
             }
-            if (nums[j] == target && ans[1] == -1) {
-                ans[1] = j;
-            }
-
-            i++;
-            j--;
-
-            if (ans[0] != -1 && ans[1] != -1) {
-                break;
+        }
+        i = 0;
+        j = nums.length - 1;
+        while (i <= j) {
+            int mid = (i + j) / 2;
+            if (nums[mid] == target) {
+                i = mid + 1;
+                ans[1] = mid;
+            } else if (nums[i] < target) {
+                i = mid + 1;
+            } else {
+                j = mid - 1;
             }
         }
 
