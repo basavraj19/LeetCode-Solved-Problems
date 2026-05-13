3ffe686962c3ad722ef430375825ae143a8025c5
Basavraj <111918104+basavraj19@users.noreply.github.com>
2026-05-14 02:15:13 +0530

    Time: 0 ms (100%), Space: 53.1 MB (8.53%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -3,13 +3,13 @@ class Solution {
         int low = 0, high = nums.length - 1, mid = -1;
         while (low <= high) {
             mid = (low + high) / 2;
-            if (mid < nums.length -1 && nums[mid] == nums[mid + 1]) {
+            if (mid < nums.length - 1 && nums[mid] == nums[mid + 1]) {
                 if (mid % 2 == 0) {
                     low = mid + 2;
                 } else {
                     high = mid - 1;
                 }
-            } else if (mid >0 && nums[mid - 1] == nums[mid]) {
+            } else if (mid > 0 && nums[mid - 1] == nums[mid]) {
                 if ((mid - 1) % 2 == 0) {
                     low = mid + 1;
                 } else {
@@ -20,6 +20,6 @@ class Solution {
             }
         }
 
-        return nums[mid];
+        return -1;
     }
 }
