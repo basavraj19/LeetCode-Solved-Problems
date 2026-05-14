9586a7f59f9ede886400801ca0fe5028e6f521fc
Basavraj <111918104+basavraj19@users.noreply.github.com>
2026-05-14 20:40:36 +0530

    Time: 0 ms (100%), Space: 52.9 MB (37.96%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -3,21 +3,29 @@ class Solution {
         if (nums.length == 1) {
             return nums[0];
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (i == 0) {
-                if (nums[i] != nums[i + 1]) {
-                    return nums[i];
-                }
-            } else if (i == nums.length - 1) {
-                if (nums[i] != nums[i - 1]) {
-                    return nums[i];
-                }
+
+        if (nums[0] != nums[1]) {
+            return nums[0];
+        }
+
+        if (nums[nums.length - 1] != nums[nums.length - 2]) {
+            return nums[nums.length - 1];
+        }
+
+        int i = 1, j = nums.length - 2;
+        while (i <= j) {
+            int mid = (i + j) / 2;
+            if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]) {
+                return nums[mid];
+            }
+
+            if ((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || (mid % 2 == 1 && nums[mid] == nums[mid - 1])) {
+                i = mid + 1;
             } else {
-                if (!(nums[i] == nums[i + 1] || nums[i] == nums[i - 1])) {
-                    return nums[i];
-                }
+                j = mid - 1;
             }
         }
+
         return -1;
     }
 }
