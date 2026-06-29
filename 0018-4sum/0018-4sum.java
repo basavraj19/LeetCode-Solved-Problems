af2f72e53aa2656fe0054b89f440eb125854f2c1
Basavraj Birajdar <111918104+basavraj19@users.noreply.github.com>
2026-06-30 00:54:32 +0530

    Time: 508 ms (5.02%), Space: 47.5 MB (5.26%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,5 +1,6 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
         int n = nums.length;
         for (int i = 0; i < n; i++) {
@@ -10,12 +11,11 @@ class Solution {
                     long val = (long) target - s;
                     if (map.containsKey(val)) {
                         List<Integer> arr = new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[k], (int) val));
-                        Collections.sort(arr);
                         if (!ans.contains(arr)) {
                             ans.add(arr);
                         }
                     }
-                    map.put((long)nums[k], k);
+                    map.put((long) nums[k], k);
                 }
             }
         }
