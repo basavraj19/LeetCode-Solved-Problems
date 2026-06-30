4bdcef9ca31dd147e598e1c62be87f26eafa43d6
Basavraj Birajdar <111918104+basavraj19@users.noreply.github.com>
2026-07-01 00:38:42 +0530

    Time: 0 ms (100%), Space: 47.9 MB (86.33%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,32 +1,39 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] ans = new int[] { -1, -1 };
-        int i = 0, j = nums.length - 1;
-        while (i <= j) {
-            int mid = (i + j) / 2;
-            if (nums[mid] == target) {
-                j = mid - 1;
-                ans[0] = mid;
-            } else if (nums[mid] > target) {
-                j = mid - 1;
-            } else {
-                i = mid + 1;
-            }
-        }
-        i = 0;
-        j = nums.length - 1;
-        while (i <= j) {
-            int mid = (i + j) / 2;
-            if (nums[mid] == target) {
-                i = mid + 1;
-                ans[1] = mid;
-            } else if (nums[mid] < target) {
-                i = mid + 1;
-            } else {
-                j = mid - 1;
-            }
-        }
+        int firstInd = getFirstOccurance(nums, target);
+        int lastInd = getLastOccurance(nums, target);
+        return new int[] { firstInd, lastInd };
+    }
 
-        return ans;
+    public int getFirstOccurance(int a[], int target) {
+        int i = 0, j = a.length - 1, ind = -1;
+        while (i <= j) {
+            int mid = (i + j) / 2;
+            if (target == a[mid]) {
+                ind = mid;
+                j = mid - 1;
+            } else if (target > a[mid]) {
+                i = mid + 1;
+            } else {
+                j = mid - 1;
+            }
+        }
+        return ind;
+    }
+
+    public int getLastOccurance(int a[], int target) {
+        int i = 0, j = a.length - 1, ind = -1;
+        while (i <= j) {
+            int mid = (i + j) / 2;
+            if (target == a[mid]) {
+                ind = mid;
+                i = mid + 1;
+            } else if (target > a[mid]) {
+                i = mid + 1;
+            } else {
+                j = mid - 1;
+            }
+        }
+        return ind;
     }
 }
