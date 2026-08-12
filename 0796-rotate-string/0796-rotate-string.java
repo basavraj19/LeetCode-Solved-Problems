8611ecdd9390b45d0efb48889ab0d45b0f6f8e0a
Basavraj Birajdar <111918104+basavraj19@users.noreply.github.com>
2026-08-12 15:08:48 +0530

    Time: 1 ms (85.8%), Space: 43.3 MB (71.35%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,6 +1,26 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
 
-        return s.length() == goal.length() &&  (s+s).contains(goal);
+        String str = s + s;
+        for (int i = 0; i < s.length(); i++) {
+            if (check(str, i, goal)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean check(String str, int ind, String goal) {
+        int i = 0;
+        while (i < goal.length()) {
+            if (str.charAt(i + ind) != goal.charAt(i)) {
+                return false;
+            }
+            i++;
+        }
+        return true;
     }
 }
