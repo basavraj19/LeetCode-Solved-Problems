cbfbc5fe2267902091a2116778666918c05d7770
Basavraj Birajdar <111918104+basavraj19@users.noreply.github.com>
2026-08-12 23:57:42 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (27.72%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,26 +1,26 @@
 class Solution {
+
     public boolean rotateString(String s, String goal) {
         if (s.length() != goal.length()) {
             return false;
         }
+        int n = s.length();
 
-        String str = s + s;
-        for (int i = 0; i < s.length(); i++) {
-            if (check(str, i, goal)) {
+        for (int rotation = 0; rotation < n; rotation++) {
+            boolean flag = true;
+            for (int i = 0; i < n; i++) {
+                if (goal.charAt((i + rotation) % n) != s.charAt(i)) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag)
                 return true;
-            }
-        }
-        return false;
-    }
 
-    public boolean check(String str, int ind, String goal) {
-        int i = 0;
-        while (i < goal.length()) {
-            if (str.charAt(i + ind) != goal.charAt(i)) {
-                return false;
-            }
-            i++;
         }
-        return true;
+
+        return false;
+
     }
 }
