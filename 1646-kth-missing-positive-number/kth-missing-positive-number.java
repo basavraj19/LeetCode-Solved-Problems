a5b1bc05c953c057d4ec45eb3d23aa13b38c8eaa
Basavraj Birajdar <111918104+basavraj19@users.noreply.github.com>
2026-08-13 23:57:59 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (11.95%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -1,21 +1,15 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int max = 0;
-        for (int i = 0; i < arr.length; i++) {
-            max = Math.max(max, arr[i]);
-            map.put(arr[i], 1);
-        }
-
-        for (int i = 1; i <= max; i++) {
-            if (!map.containsKey(i)) {
-                k--;
-                if (k == 0) {
-                    return i;
-                }
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int missingNoCount = arr[mid] - (mid + 1);
+            if (missingNoCount >=  k) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
-
-        return max + k;
+        return low + k;
     }
 }
