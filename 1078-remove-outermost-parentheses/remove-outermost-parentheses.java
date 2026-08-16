f094642b443903ca14d1db79a05f759633652669
Basavraj Birajdar <111918104+basavraj19@users.noreply.github.com>
2026-08-16 21:07:41 +0530

    Time: 3 ms (61.47%) | Memory: 43.8 MB (19.04%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,23 +1,26 @@
 class Solution {
     public String removeOuterParentheses(String s) {
+        if(s.length() < 3){
+            return "";
+        }
         int c = 0;
-        String ans = "";
+        StringBuilder ans = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
             if (c == 0) {
                 c++;
             } else {
                 if (s.charAt(i) == '(') {
                     c++;
-                    ans += s.charAt(i);
+                    ans.append('(');
                 } else {
                     c--;
                     if (c != 0) {
-                        ans += s.charAt(i);
+                       ans.append(')');
                     }
                 }
             }
         }
 
-        return ans;
+        return ans.toString();
     }
 }
