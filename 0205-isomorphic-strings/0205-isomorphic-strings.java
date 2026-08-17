2437f7337c5e347baaef65c22581c1e97b2c0ff2
Basavraj Birajdar <111918104+basavraj19@users.noreply.github.com>
2026-08-17 23:21:49 +0530

    Time: 5 ms (98.71%), Space: 44.1 MB (26.55%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,24 +1,29 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        int[] map = new int[256];
-        int[] taken = new int[256];
-        
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int a[] = new int[256];
+        int b[] = new int[256];
+
         for (int i = 0; i < s.length(); i++) {
-            int asciiCode = (int) s.charAt(i);
-            if (map[asciiCode] == 0) {
-                int ind = (int) t.charAt(i);
-                if (taken[ind] == 0) {
-                    taken[ind] = asciiCode;
-                    map[asciiCode] = ind;
+            int asciiCodeS = (int) s.charAt(i);
+            if (a[asciiCodeS] == 0) {
+                int asciiCodeT = (int) t.charAt(i);
+                if (b[asciiCodeT] == 0) {
+                    b[asciiCodeT] = 1;
+                    a[asciiCodeS] = asciiCodeT;
                 } else {
                     return false;
                 }
             } else {
-                if (t.charAt(i) != (char) map[asciiCode]) {
+                char ch = (char) a[asciiCodeS];
+                if (t.charAt(i) != ch) {
                     return false;
                 }
             }
         }
+
         return true;
     }
 }
