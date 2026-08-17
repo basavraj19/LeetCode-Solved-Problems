826dca5c0af1e980e6b374ca74da00e276dafd5e
Basavraj Birajdar <111918104+basavraj19@users.noreply.github.com>
2026-08-17 23:26:37 +0530

    Time: 5 ms (98.71%) | Memory: 43.4 MB (98.49%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,27 +1,18 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
-        int a[] = new int[256];
-        int b[] = new int[256];
+        int[] sMap = new int[256];
+        int[] tMap = new int[256];
 
         for (int i = 0; i < s.length(); i++) {
-            int asciiCodeS = (int) s.charAt(i);
-            if (a[asciiCodeS] == 0) {
-                int asciiCodeT = (int) t.charAt(i);
-                if (b[asciiCodeT] == 0) {
-                    b[asciiCodeT] = 1;
-                    a[asciiCodeS] = asciiCodeT;
-                } else {
-                    return false;
-                }
-            } else {
-                char ch = (char) a[asciiCodeS];
-                if (t.charAt(i) != ch) {
-                    return false;
-                }
+            char sc = s.charAt(i);
+            char tc = t.charAt(i);
+
+            if (sMap[sc] != tMap[tc]) {
+                return false;
             }
+
+            sMap[sc] = i + 1;
+            tMap[tc] = i + 1;
         }
 
         return true;
