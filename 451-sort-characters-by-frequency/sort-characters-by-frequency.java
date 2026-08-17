ca3655be4711ad8fa640a3be95d72ccef6b90277
Basavraj Birajdar <111918104+basavraj19@users.noreply.github.com>
2026-08-18 00:07:04 +0530

    Time: 8 ms (90.20%) | Memory: 45.9 MB (99.34%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,31 +1,30 @@
 class Solution {
     public String frequencySort(String s) {
-        Map<Character, Integer> map = new HashMap<>();
+        int a[][] = new int[256][2];
         StringBuilder ans = new StringBuilder();
 
         for (int i = 0; i < s.length(); i++) {
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            int k = (int) s.charAt(i);
+            a[k][0] = k;
+            a[k][1]++;
         }
 
-        Map<Character, Integer> sorted = map.entrySet()
-                .stream()
-                .sorted(Map.Entry.<Character, Integer>comparingByValue().reversed())
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        Map.Entry::getValue,
-                        (a, b) -> a,
-                        LinkedHashMap::new));
+        Arrays.sort(a, (p, q) -> Integer.compare(q[1], p[1]));
 
-        for (Map.Entry<Character, Integer> item : sorted.entrySet()) {
-            ans.append(solve(item.getKey(), item.getValue()));
+        int j = 0;
+        while (a[j][1] != 0) {
+            ans.append(solve(a[j][0], a[j][1]));
+            j++;
         }
+
         return ans.toString();
     }
 
-    public StringBuilder solve(Character ch, int n) {
+    public StringBuilder solve(int asciiCode, int n) {
         StringBuilder ans = new StringBuilder();
 
         for (int i = 0; i < n; i++) {
+            char ch = (char) asciiCode;
             ans.append(ch);
         }
 
