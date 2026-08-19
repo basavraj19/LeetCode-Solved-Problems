2c71e9b5893ae3a7063ab1a721a71aea77434245
Basavraj Birajdar <111918104+basavraj19@users.noreply.github.com>
2026-08-19 19:53:37 +0530

    Time: 0 ms (100%), Space: 43 MB (34.42%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
@@ -1,13 +1,12 @@
 class Solution {
     public int maxDepth(String s) {
-        Stack<Character> stack = new Stack<>();
-        int ans = 0;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i) == '('){
-                stack.push('(');
-                ans = Math.max(ans, stack.size());
-            }else if(s.charAt(i) == ')'){
-                stack.pop();
+        int ans = 0, count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                count++;
+                ans = Math.max(ans, count);
+            } else if (s.charAt(i) == ')') {
+                count--;
             }
         }
 
