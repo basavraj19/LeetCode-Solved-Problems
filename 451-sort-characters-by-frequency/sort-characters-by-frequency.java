a1e4868b945d40961d1e32802ecff9e534caa389
Basavraj Birajdar <111918104+basavraj19@users.noreply.github.com>
2026-08-24 21:33:50 +0530

    Time: 9 ms (89.52%) | Memory: 46.6 MB (64.48%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,23 +1,22 @@
 class Solution {
     public String frequencySort(String s) {
-        StringBuilder ans = new StringBuilder();
         int a[][] = new int[123][2];
 
         for (int i = 0; i < s.length(); i++) {
-            int asciiCode = (int) s.charAt(i);
-            a[asciiCode][0] = asciiCode;
-            a[asciiCode][1]++;
+            int ind = (int) s.charAt(i);
+            a[ind - 1][0] = ind;
+            a[ind - 1][1]++;
         }
 
         Arrays.sort(a, (p, q) -> Integer.compare(q[1], p[1]));
 
-        for (int i = 0; i < 123; i++) {
-            if (a[i][1] == 0) {
-                break;
-            }
+        StringBuilder ans = new StringBuilder();
+        int i = 0;
+        while (a[i][1] != 0) {
             for (int j = 0; j < a[i][1]; j++) {
                 ans.append((char) a[i][0]);
             }
+            i++;
         }
 
         return ans.toString();
