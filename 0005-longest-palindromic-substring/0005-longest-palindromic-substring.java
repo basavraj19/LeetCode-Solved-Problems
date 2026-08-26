8a20d6ebf94f25d92ba77e7a881c9ab37abce4c1
Basavraj Birajdar <111918104+basavraj19@users.noreply.github.com>
2026-08-26 15:00:14 +0530

    Time: 19 ms (41.1%), Space: 45.4 MB (43.03%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -4,39 +4,32 @@ class Solution {
 
         for (int i = 0; i < s.length(); i++) {
             int l = i, r = i + 1;
-            StringBuilder arr = new StringBuilder();
             while (l >= 0 && r < s.length()) {
                 if (s.charAt(l) != s.charAt(r)) {
                     break;
                 }
-                arr.insert(0, s.charAt(l));
-                arr.append(s.charAt(r));
                 l--;
                 r++;
             }
-
-            if (arr.length() > ans.length()) {
+            
+            if ((r - l - 1) > ans.length()) {
                 ans = new StringBuilder();
-                ans.append(arr);
+                ans.append(s, l + 1, r);
             }
 
             l = i - 1;
             r = i + 1;
-            arr = new StringBuilder();
-            arr.append(s.charAt(i));
             while (l >= 0 && r < s.length()) {
                 if (s.charAt(l) != s.charAt(r)) {
                     break;
                 }
-                arr.insert(0, s.charAt(l));
-                arr.append(s.charAt(r));
                 l--;
                 r++;
             }
 
-            if (arr.length() > ans.length()) {
+            if ((r - l - 1)  > ans.length()) {
                 ans = new StringBuilder();
-                ans.append(arr);
+                ans.append(s, l + 1, r);
             }
         }
 
