3bbd7aee74c3e23376f91b559714c2bfa937844f
Basavraj Birajdar <111918104+basavraj19@users.noreply.github.com>
2026-08-26 14:49:41 +0530

    Time: 102 ms (30.66%) | Memory: 47.6 MB (10.30%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,29 +1,45 @@
 class Solution {
     public String longestPalindrome(String s) {
         StringBuilder ans = new StringBuilder();
+
         for (int i = 0; i < s.length(); i++) {
-            for (int j = i; j < s.length(); j++) {
-                if (isPalindrom(s, i, j)) {
-                    int t = j - i + 1;
-                    if (t > ans.length()) {
-                        ans = new StringBuilder();
-                        ans.append(s, i, j + 1);
-                    }
+            int l = i, r = i + 1;
+            StringBuilder arr = new StringBuilder();
+            while (l >= 0 && r < s.length()) {
+                if (s.charAt(l) != s.charAt(r)) {
+                    break;
                 }
+                arr.insert(0, s.charAt(l));
+                arr.append(s.charAt(r));
+                l--;
+                r++;
+            }
+
+            if (arr.length() > ans.length()) {
+                ans = new StringBuilder();
+                ans.append(arr);
+            }
+
+            l = i - 1;
+            r = i + 1;
+            arr = new StringBuilder();
+            arr.append(s.charAt(i));
+            while (l >= 0 && r < s.length()) {
+                if (s.charAt(l) != s.charAt(r)) {
+                    break;
+                }
+                arr.insert(0, s.charAt(l));
+                arr.append(s.charAt(r));
+                l--;
+                r++;
+            }
+
+            if (arr.length() > ans.length()) {
+                ans = new StringBuilder();
+                ans.append(arr);
             }
         }
 
         return ans.toString();
-    }
-
-    public boolean isPalindrom(String s, int i, int j) {
-        while (i <= j) {
-            if (s.charAt(i) != s.charAt(j)) {
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
     }
 }
