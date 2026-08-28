b446d799d964f727df0aee55e9144497a594497c
Basavraj Birajdar <111918104+basavraj19@users.noreply.github.com>
2026-08-28 23:00:38 +0530

    Time: 505 ms (26.46%), Space: 47 MB (9.83%) - LeetHub

---
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int beautySum(String s) {
+        int ans = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                ans += getSubstringBeautySum(s, i, j);
+            }
+        }
+
+        return ans;
+    }
+
+    private int getSubstringBeautySum(String s, int low, int high) {
+        int a[] = new int[26];
+        for (int i = low; i <= high; i++) {
+            int ind = (int) s.charAt(i) - 'a';
+            a[ind]++;
+        }
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for (int i = 0; i < 26; i++) {
+            if (a[i] == 0) {
+                continue;
+            }
+            min = Math.min(min, a[i]);
+            max = Math.max(max, a[i]);
+        }
+        return max - min;
+    }
+}
