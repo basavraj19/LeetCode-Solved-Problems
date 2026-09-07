aceb09f375e15045765cd55f47eeae9165a8c366
Basavraj Birajdar <111918104+basavraj19@users.noreply.github.com>
2026-09-08 00:15:18 +0530

    Time: 0 ms (100%), Space: 46.7 MB (50.14%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -15,15 +15,21 @@ public class Solution {
             return null;
         }
 
-        Map<ListNode, Integer> map = new HashMap<>();
-        ListNode temp = head;
+        ListNode slow = head;
+        ListNode fast = head;
 
-        while (temp != null) {
-            if (map.containsKey(temp)) {
-                return temp;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
             }
-            map.put(temp, 1);
-            temp = temp.next;
         }
         return null;
     }
