4e939a9cf23a0729b701bbbee2037ecc1283c026
Basavraj Birajdar <111918104+basavraj19@users.noreply.github.com>
2026-09-08 00:02:57 +0530

    Time: 0 ms (100%), Space: 46.8 MB (33.65%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -16,15 +16,14 @@ public class Solution {
         }
 
         ListNode slow = head;
-        ListNode fast = head.next;
+        ListNode fast = head;
 
         while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
             if (slow == fast) {
                 return true;
             }
-
-            slow = slow.next;
-            fast = fast.next.next;
         }
 
         return false;
